05005194e664ac6755603f021709082455fba523
Gaallard <gallardoagustineduardo@gmail.com>
2024-07-14 16:54:55 -0300

    asfsd

---
+++ frontend/src/components/MyCourses/Mycurses.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Mycurses.css';
+
+const Mycurses = () => {
+  return (
+    <div className="container">
+      <h1>Mis Cursos</h1>
+      <div className="courses-grid">
+      </div>
+    </div>
+  );
+};
+
+export default Mycurses;
+
+
+/*
+        <div className="course-card">
+          <h2>Nombre del Curso</h2>
+          <p>Descripción breve del curso. Aquí va el contenido del curso seleccionado.</p>
+        </div>
+*/
